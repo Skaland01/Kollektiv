fffc28e28220c6c8400790899f241d82b62b8092
Skaland01 <evenjoruds@gmail.com>
2025-01-07 19:56:39 +0100

    Initial Commit

---
+++ Kollektiv/KollektivApp.swift
@@ -0,0 +1,17 @@
+//
+//  KollektivApp.swift
+//  Kollektiv
+//
+//  Created by Even Skåland on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct KollektivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
